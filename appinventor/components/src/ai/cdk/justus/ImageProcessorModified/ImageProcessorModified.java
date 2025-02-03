1ac9f9642cf8d33a2126e6fddeb6c6137c3bdfcc
Brandon Ang <angn9096@gmail.com>
2025-02-03 18:57:16 +0800

    Update ImageProcessorModified.java

--- appinventor/components/src/ai/cdk/justus/ImageProcessorModified/ImageProcessorModified.java
+++ appinventor/components/src/ai/cdk/justus/ImageProcessorModified/ImageProcessorModified.java
@@ -1,3 +1,11 @@
+/* Extension created by Justus, modified by Bextdev
+ * 
+ * Licensed under Apache License V2
+ *
+ * No conflicts with the original extension
+ *
+ */
+
 package ai.cdk.justus.ImageProcessorModified;
 
 import java.io.ByteArrayOutputStream;
@@ -25,11 +33,11 @@ import java.util.Date;
     description = "A component for complex image processing in MIT App Inventor.",
     category = ComponentCategory.EXTENSION,
     nonVisible = true,
-    iconName = "images/extension.png")
+    iconName = "aiwebres/icon.png")
 @SimpleObject(external = true)
 public class ImageProcessorModified extends AndroidNonvisibleComponent implements Component {
 
-    public static final String VERSION = "1.1";
+    public static final String VERSION_NAME = "1.1";
     public static final float DEFAULT_WEIGHT = 0.5f;
     private ComponentContainer container;
     private float weight = DEFAULT_WEIGHT;
